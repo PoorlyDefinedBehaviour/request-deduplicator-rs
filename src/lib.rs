7eac8b7bb71b1a78daae2db0fa703a1fb533a108
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2023-04-11 21:41:05 -0300

    handle race condition

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, Context, Result};
 use crossbeam_skiplist::SkipMap;
 
 use std::{future::Future, sync::Arc};
@@ -7,18 +7,25 @@ use tokio::sync::broadcast;
 const BROADCAST_CHANNEL_CAPACITY: usize = 1;
 
 #[derive(Debug, Clone)]
-pub struct RequestDeduplicator<K, V>
+pub struct RequestDeduplicator<I, K, V>
 where
-    K: Ord + Send + 'static,
-    V: Clone + Send + 'static,
+    K: Ord,
 {
-    requests_in_flight: Arc<SkipMap<K, broadcast::Receiver<V>>>,
+    requests_in_flight: Arc<SkipMap<K, Task<I, V>>>,
+    // requests_in_flight: Arc<RwLock<HashMap<K, broadcast::Receiver<V>>>>,
 }
 
-impl<K, V> RequestDeduplicator<K, V>
+#[derive(Debug)]
+struct Task<I, V> {
+    id: I,
+    receiver: broadcast::Receiver<V>,
+}
+
+impl<I, K, V> RequestDeduplicator<I, K, V>
 where
     K: Ord + Send + 'static,
     V: Clone + Send + 'static,
+    I: PartialEq + Send + Clone + 'static,
 {
     pub fn new() -> Self {
         Self {
@@ -26,38 +33,51 @@ where
         }
     }
 
-    pub async fn dedup<F>(&self, key: K, future: F) -> Result<V>
+    pub async fn dedup<F>(&self, key: K, id: I, future: F) -> Result<V>
     where
         F: Future<Output = V>,
+        I: std::fmt::Debug,
+        K: std::fmt::Debug,
     {
-        match self.requests_in_flight.get(&key) {
-            None => {
-                let (sender, receiver) = broadcast::channel(BROADCAST_CHANNEL_CAPACITY);
+        let (sender, receiver) = broadcast::channel(BROADCAST_CHANNEL_CAPACITY);
 
-                let entry = self.requests_in_flight.insert(key, receiver);
+        let entry = self.requests_in_flight.get_or_insert(
+            key,
+            Task {
+                id: id.clone(),
+                receiver,
+            },
+        );
 
-                let result = future.await;
+        // If the entry was inserted by this task it means that
+        // this task is the task that will poll the future.
+        if entry.value().id == id {
+            let result = future.await;
 
-                let _ = entry.remove();
+            // Yield so other tasks have a chance of resubscribing to the receiver.
+            tokio::task::yield_now().await;
 
-                if let Err(err) = sender.send(result.clone()) {
-                    return Err(anyhow!(
-                        "erro broadcasting result to listeners. err={}",
-                        err.to_string()
-                    ));
-                }
-                Ok(result)
-            }
-            Some(result_rx) => {
-                let value = result_rx
-                .value()
-                .resubscribe()
-                .recv()
-                .await
-                .expect("bug: the sender will never be closed before sending a value, a value should always be available at some point in the future");
-                Ok(value)
-            }
+            sender
+                .send(result.clone())
+                .map_err(|err| anyhow!("error broadcasting task result: {}", err.to_string()))?;
+
+            let _ = entry.remove();
+            return Ok(result);
         }
+        // Some other task will make the request so this task should just await for the result.
+        let mut receiver = entry.value().receiver.resubscribe();
+        let result = match receiver
+            .recv()
+            .await
+            .context("trying to receive result from channel")
+        {
+            Err(_) => {
+                // The channel was closed, execute the future to get the result.
+                future.await
+            }
+            Ok(v) => v,
+        };
+        Ok(result)
     }
 }
 
@@ -70,27 +90,38 @@ mod tests {
 
     use super::*;
 
-    #[tokio::test]
-    async fn todo() {
+    #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+    async fn basic() {
         const NUM_TASKS: usize = 10_000;
 
-        let deduplicator = RequestDeduplicator::new();
+        let deduplicator = Arc::new(RequestDeduplicator::new());
 
-        let request_executions = AtomicUsize::new(0);
+        let request_executions = Arc::new(AtomicUsize::new(0));
 
-        let tasks = (0..NUM_TASKS).into_iter().map(|_| {
-            deduplicator.dedup("key", async {
-                tokio::time::sleep(Duration::from_millis(100)).await;
-                let _ = request_executions.fetch_add(1, Ordering::SeqCst);
-            })
-        });
+        let mut handles = Vec::with_capacity(NUM_TASKS);
 
-        for result in futures::future::join_all(tasks).await {
-            result.expect("deduplicator task returned error");
+        for id in 0..NUM_TASKS {
+            let deduplicator = deduplicator.clone();
+            let request_executions = Arc::clone(&request_executions);
+
+            handles.push(tokio::spawn(async move {
+                let _ = deduplicator
+                    .dedup("key", id, async move {
+                        let _ = request_executions.fetch_add(1, Ordering::SeqCst);
+                        tokio::time::sleep(Duration::from_secs(1)).await;
+                    })
+                    .await;
+            }));
+        }
+
+        for handle in handles {
+            handle.await.unwrap();
         }
 
         // Only one request should have been executed. The other requests using the same key that
         // arrived while the first request was executing should wait for the result of the first request.
         assert_eq!(1, request_executions.load(Ordering::SeqCst),);
+
+        assert!(deduplicator.requests_in_flight.is_empty());
     }
 }
